ec162fedb965ec68c8cba9362a0404cc80e3c5cc
Vincent Pfister <vincent.pfister@raisepartner.com>
2022-04-20 23:06:42 +0200

    add power decomposition

--- src/power.spec.ts
+++ src/power.spec.ts
@@ -1,9 +1,3 @@
-/**
- * compute power <-> speed, taking gravity, drag and rolling resistance into account
- * 
- * inspired by https://www.gribble.org/cycling/power_v_speed.html
- */
-
 import { EnvParams, Params } from "./params";
 import { PowerCalulator } from "./power";
 
@@ -11,7 +5,8 @@ describe("PowerCalculator", () => {
   const params = new Params(75, 7);
   const calculator = new PowerCalulator(params);
   const envFlat = new EnvParams();
-  const envUp = new EnvParams(0.05);
+  const envUphill = new EnvParams(0.05);
+  const envDownhill = new EnvParams(-0.05);
 
   describe("calculateForces", () => {
     it("should compute gravity=0 with no gradient", () => {
@@ -19,27 +14,65 @@ describe("PowerCalculator", () => {
       expect(gravity).toEqual(0);
     });
     it("should compute gravity with a gradient", () => {
-      const { gravity } = calculator.calculateForces(30, envUp);
+      const { gravity } = calculator.calculateForces(30, envUphill);
       expect(gravity).toBeCloseTo(40.16);
     });
     it("should compute rolling resistance", () => {
-      const { rolling } = calculator.calculateForces(30, envUp);
+      const { rolling } = calculator.calculateForces(30, envUphill);
       expect(rolling).toBeCloseTo(4.02);
     });
     it("should compute rolling resistance when v<0", () => {
-      const { rolling } = calculator.calculateForces(-30, envUp);
+      const { rolling } = calculator.calculateForces(-30, envUphill);
       expect(rolling).toBeCloseTo(-4.02);
     });
     it("should compute drag", () => {
-      const { drag } = calculator.calculateForces(30, envUp);
+      const { drag } = calculator.calculateForces(30, envUphill);
       expect(drag).toBeCloseTo(13.65);
     });
   });
 
-  describe('calculatePower', () => {
+  describe("calculate power flat", () => {
     const power = calculator.calculatePower(30);
     it("should compute leg power", () => {
       expect(power.leg).toBeCloseTo(150.27);
     });
-  })
+    it("should compute wheel power", () => {
+      expect(power.wheel).toBeCloseTo(147.26);
+    });
+    it("should compute drivetrain power loss", () => {
+      expect(power.drivetrainLoss).toBeCloseTo(3.01);
+    });
+    it("should compute gravity power", () => {
+      expect(power.gravity).toBeCloseTo(0);
+    });
+    it("should compute rolling power", () => {
+      expect(power.rolling).toBeCloseTo(33.51);
+    });
+    it("should compute drag power", () => {
+      expect(power.drag).toBeCloseTo(113.76);
+    });
+  });
+
+  describe("calculate power uphill", () => {
+    const power = calculator.calculatePower(20, envUphill);
+    it("should compute leg power", () => {
+      expect(power.leg).toBeCloseTo(284.81);
+    });
+    it("should compute braking power", () => {
+      expect(power.braking).toEqual(0);
+    });
+    it("should compute gravity power", () => {
+      expect(power.gravity).toBeCloseTo(223.1);
+    });
+  });
+
+  describe("calculate power downhill", () => {
+    const power = calculator.calculatePower(40, envDownhill);
+    it("should compute leg power", () => {
+      expect(power.leg).toBeCloseTo(-131.93);
+    });
+    it("should compute braking power", () => {
+      expect(power.braking).toBeCloseTo(131.93);
+    });
+  });
 });
--- src/power.ts
+++ src/power.ts
@@ -1,3 +1,9 @@
+/**
+ * compute power <-> speed, taking gravity, drag and rolling resistance into account
+ *
+ * inspired by https://www.gribble.org/cycling/power_v_speed.html
+ */
+
 import { DefaultEnvParams, EnvParams, Params } from "./params";
 
 const GRAVITY = 9.8067;
@@ -5,9 +11,9 @@ const GRAVITY = 9.8067;
 class Forces {
   /**
    * forces applied to the system (in N)
-   * @param gravity 
-   * @param rolling 
-   * @param drag 
+   * @param gravity
+   * @param rolling
+   * @param drag
    */
   constructor(
     public readonly gravity: number,
@@ -15,7 +21,7 @@ class Forces {
     public readonly drag: number
   ) {}
 
-  get total() : number {
+  get total(): number {
     return this.gravity + this.rolling + this.drag;
   }
 }
@@ -26,6 +32,9 @@ class Power {
     public readonly wheel: number,
     public readonly drivetrainLoss: number,
     public readonly braking: number,
+    public readonly gravity: number,
+    public readonly rolling: number,
+    public readonly drag: number
   ) {}
 }
 
@@ -57,13 +66,23 @@ export class PowerCalulator {
 
   calculatePower(velocity: number, env: EnvParams = DefaultEnvParams): Power {
     const forces = this.calculateForces(velocity, env);
-    const wheelPower = forces.total * velocity * 1000 / 3600;
-    const drivetrainFraction = (wheelPower > 0)
-      ? 1 - this.params.drivetrainLoss
-      : 1
+    const wheelPower = (forces.total * velocity * 1000) / 3600;
+    const drivetrainFraction =
+      wheelPower > 0 ? 1 - this.params.drivetrainLoss : 1;
     const legPower = wheelPower / drivetrainFraction;
-    return (legPower > 0)
-      ? new Power(legPower, wheelPower, legPower - wheelPower, 0)
-      : new Power(legPower, wheelPower, 0, -legPower);
+    const [dtl, braking] =
+      legPower > 0 ? [legPower - wheelPower, 0] : [0, -legPower];
+    const gravityPower = forces.gravity * velocity / (3600 / 1000);
+    const rollingPower = forces.rolling * velocity / (3600 / 1000);
+    const dragPower = forces.drag * velocity / (3600 / 1000);
+    return new Power(
+      legPower,
+      wheelPower,
+      dtl,
+      braking,
+      gravityPower,
+      rollingPower,
+      dragPower
+    );
   }
 }
